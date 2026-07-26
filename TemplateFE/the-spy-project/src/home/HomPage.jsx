296be94fcec427c54adab31c10996c34b111ea50
poromvp <quangkietle382@gmail.com>
2026-07-26 11:44:56 +0700

    innit prj

---
+++ TemplateFE/the-spy-project/src/home/HomPage.jsx
@@ -0,0 +1,9 @@
+export function HomePage(){
+    return(
+        <>
+            <title>Who Is Spy</title>
+            <button>Create Room</button>
+            <button>JoinRoom</button>
+        </>
+    );
+}
