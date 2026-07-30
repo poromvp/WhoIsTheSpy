cc34e52c37ecc20c6a2b2ce5a373d58d23c7f4a1
poromvp <quangkietle382@gmail.com>
2026-07-30 23:15:48 +0700

    Seperator to HomePageButton component

---
+++ TemplateFE/the-spy-project/src/home/HomePageButton.jsx
@@ -0,0 +1,21 @@
+export function HomePageButton() {
+    return (
+        <>
+            <button class="btn btn-primary">
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="12" y1="5" x2="12" y2="19"></line>
+                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                </svg>
+                Create Room
+            </button>
+
+            <button class="btn btn-secondary">
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                </svg>
+                Join Room
+            </button>
+        </>
+    );
+}
