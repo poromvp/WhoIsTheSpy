c381f931e1cc54517c10a64d399d46f9e61332ec
poromvp <quangkietle382@gmail.com>
2026-07-30 23:14:44 +0700

    Seperator to HomePageHeader component

---
+++ TemplateFE/the-spy-project/src/home/HomePageHeader.jsx
@@ -0,0 +1,17 @@
+export function HomePageHeader() {
+    return (
+        <>
+            <header class="hero-section">
+                <div class="hero-image-placeholder">
+                    {/*Chỗ này trong dự án thật bạn sẽ dùng thẻ <img src="spy-illustration.png">*/}
+                        [ Illustration Area ]
+                </div>
+
+                <div class="title-container">
+                    <h1>WHO IS <span>SPY?</span></h1>
+                    <p>The Ultimate Party Deduction Game</p>
+                </div>
+            </header>
+        </>
+    );
+}
