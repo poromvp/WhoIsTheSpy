c7c2f4a33b9f6fc48d1d0012b801ec20f0042b4e
poromvp <quangkietle382@gmail.com>
2026-07-30 23:02:10 +0700

    Seperator to PlayerStatusBar component

--- TemplateFE/the-spy-project/src/player/PlayerPage.jsx
+++ TemplateFE/the-spy-project/src/player/PlayerPage.jsx
@@ -1,8 +1,16 @@
-export function PlayerPage(){
-    return(
+export function PlayerPage() {
+    return (
         <>
             <div class="mobile-container">
-                
+                <div class="status-bar">
+                    <div class="status-header">
+                        <div class="status-title">Discussion Phase Active</div>
+                        <div class="timer">01:45</div>
+                    </div>
+                    <div class="progress-bar-bg">
+                        <div class="progress-bar-fill"></div>
+                    </div>
+                </div>
             </div>
         </>
     );
---
+++ TemplateFE/the-spy-project/src/player/PlayerStatusBar.jsx
@@ -0,0 +1,7 @@
+export function PlayerStatusBar(){
+    return(
+        <>
+            
+        </>
+    );
+}
