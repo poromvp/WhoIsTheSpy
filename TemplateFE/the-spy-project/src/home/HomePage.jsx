69de15b5f651b4f5b703b09cf8b70d8e19956cb5
poromvp <quangkietle382@gmail.com>
2026-07-31 19:14:31 +0700

    refactor HomePageButton component

--- TemplateFE/the-spy-project/src/home/HomePage.jsx
+++ TemplateFE/the-spy-project/src/home/HomePage.jsx
@@ -1,5 +1,6 @@
 import './HomePage.css';
 import {HomePageHeader} from "./HomePageHeader";
+import {HomePageButton} from "./HomePageButton"
 export function HomePage() {
     return (
         <>
@@ -9,22 +10,8 @@ export function HomePage() {
                 <HomePageHeader/>
 
                 <main className="main-content">
-
-                    <button className="btn btn-primary">
-                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
-                            <line x1="12" y1="5" x2="12" y2="19"></line>
-                            <line x1="5" y1="12" x2="19" y2="12"></line>
-                        </svg>
-                        Create Roomaaa
-                    </button>
-
-                    <button className="btn btn-secondary">
-                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <circle cx="11" cy="11" r="8"></circle>dddddđ
-                            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                        </svg>
-                        Join Room
-                    </button>
+                    <HomePageButton/>
+                    
 
                     <div className="stats-card">
                         <div className="stats-icon-wrap">
