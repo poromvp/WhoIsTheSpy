edbadb33f2cf2ce903a80b257fdf14d31ae3ae15
poromvp <quangkietle382@gmail.com>
2026-07-31 19:07:26 +0700

    Seperator to HomePageRules componenet

---
+++ TemplateFE/the-spy-project/src/home/HomePageRules.jsx
@@ -0,0 +1,18 @@
+export function HomePageRules() {
+    return (
+        <>
+            <div class="rule-card">
+                <div class="rule-header">
+                    <div class="rule-icon">
+                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="var(--icon-purple)" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M21 2l-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.778 7.778 5.5 5.5 0 0 1 7.777-7.777zm0 0L15.5 7.5m0 0l3 3L22 7l-3-3m-3.5 3.5L19 4"></path>
+                        </svg>
+                    </div>
+                    <div class="rule-number">01</div>
+                </div>
+                <h3>Get Your Word</h3>
+                <p>Everyone gets a secret word. The spy gets a slightly different one, but doesn't know it!</p>
+            </div>
+        </>
+    );
+}
