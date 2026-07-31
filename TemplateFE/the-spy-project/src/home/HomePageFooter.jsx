52f640636f43b48526cf1772e65706a9a3fb4bf1
poromvp <quangkietle382@gmail.com>
2026-07-31 19:08:37 +0700

    Seperator to  HomePageFooter component

---
+++ TemplateFE/the-spy-project/src/home/HomePageFooter.jsx
@@ -0,0 +1,10 @@
+export function HomePageFooter() {
+    return (
+        <>
+            <footer class="footer">
+                <div class="footer-text">v1.4.2 // SECURITY PROTOCOL ACTIVE</div>
+                <div class="home-indicator"></div>
+            </footer>
+        </>
+    );
+}
